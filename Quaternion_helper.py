69716077c6be68ab7eee40aa248f4b557370f1ff
Sébastien Girard <sebastiengrd@gmail.com>
2021-11-03 21:51:50 -0400

    added Conjugate and multiplication of quaternion

--- Quaternion_helper.py
+++ Quaternion_helper.py
@@ -10,11 +10,20 @@ class Quaternion:
         self.c = c
         self.d = d
 
+    def conj(self):
+        return Quaternion(self.a, -self.b, -self.c, -self.d)
+
     def __add__(self, other):
         return Quaternion(self.a + other.a, self.b + other.b, self.c + other.c, self.d + other.d)
 
     def __sub__(self, other):
         return Quaternion(self.a - other.a, self.b - other.b, self.c - other.c, self.d - other.d)
+
+    def __mul__(self, other):
+        return Quaternion(self.a * other.a - self.b * other.b - self.c * other.c - self.d * other.d,
+        (self.a * other.b + self.b * other.a + self.c * other.d - self.d * other.c),
+        (self.a * other.c - self.b * other.d + self.c * other.a + self.d * other.b),
+        (self.a * other.d + self.b * other.c - self.c * other.b + self.d * other.a))
 
     def __str__(self) -> str:
         return f"({f'{self.a} + ' if not self.a == 0 else ''}{self.b}i + {self.c}j + {self.d}k)"
@@ -25,6 +34,7 @@ class Quaternion:
 
 class Point3D(Quaternion):
     def __init__(self, x, y, z) -> None:
+        super().__init__(0, x, y, z)
         self.x = x
         self.y = y
         self.z = z
